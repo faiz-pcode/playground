487309e4056877d5bc2657063fef3e770809fd34
Faiz Hasif - PCode <faiz@platinumcode.com.my>
2020-10-12 18:22:12 +0800

    Update config for horizon and websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,8 +38,8 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => false,
-                'host' => '127.0.0.1',
-                'port' => 6001,
+                'host' => env('LARAVEL_WEBSOCKETS_HOST', '127.0.0.1'),
+                'port' => env('LARAVEL_WEBSOCKETS_PORT', 6001),
                 'scheme' => 'http'
             ],
         ],
